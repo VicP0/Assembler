8de052ca4457ce8a1ce20e72ebd09591d6354b0f
VicP0 <119295494+VicP0@users.noreply.github.com>
2024-07-01 19:40:51 +0300

    Update pre_assembler.c

--- pre_assembler.c
+++ pre_assembler.c
@@ -11,3 +11,17 @@ char *non_macro_line_processing(char *str){
 int empty_line{
 
 };
+
+int check_if_macr(char* input)
+{
+    char* temp;
+    remove_spaces(input);
+    temp  = (char*)malloc((strlen(input) + 1) * sizeof(char));
+
+    temp = strncpy(temp , temp , 4);
+
+    if(valid_dec_macro(temp) > 0)
+        return 0;
+
+    return -1;
+}
