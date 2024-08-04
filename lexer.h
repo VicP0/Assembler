95f9504f23dd2ccac1e3503943eac71a8313d95c
VicP0 <119295494+VicP0@users.noreply.github.com>
2024-08-04 16:07:41 +0300

    Create lexer.h

---
+++ lexer.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "errors.h"
+
+#define REGISTER_WORD_LEN 2
+
+/* copies word from source into target with size bytes */
+void copyWord(char *source,char *target,int size);
+/* validate the number by immediate addressing format and return its size */
+int immediateAddressing(char *word);
+/* validate the number by jump addressing format and return its size */
+int jumpAdressing(char *word);
+
+void getArgument(char *source,char *target,int index);
+/* function to check if a given line is terminated correctly */
+int terminatedCorrectly(char *line,int index);
+int isLabel(char *line,int firstWordInLine);
+int isRegister(char *line);
+int validNumber(char *num);
