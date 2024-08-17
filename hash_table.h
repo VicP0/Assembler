ca83a5285e234efcb3bf9638da02d625d9e4240f
DekelMO <dekelm94@gmail.com>
2024-08-17 15:40:07 +0300

    Update hash_table.h

--- hash_table.h
+++ hash_table.h
@@ -8,8 +8,8 @@
 
 /* an item in macros' hash table */
 typedef struct hashTableItem {
-    char* key;
-    char* value;
+    char *key;
+    char *value;
     struct hashTableItem *next;
 } hashTableItem;
 
@@ -32,22 +32,32 @@ typedef struct hashTableInt {
     hashTableIntItem **items;
     int size;
 } hashTableInt;
+
 /* generating hash value withing [0, size) range to store in hash table array */
 unsigned int calculate_hash(char *input, int size);
+
 /* allocating memory for hash table array */
-int init_hash_table(hashTable* table, int size);
+int init_hash_table(hashTable *table, int size);
+
 /* getting key's value (macros' hashTableItem struct) */
-char* get_value(hashTable* table, char* key);
+char *get_value(hashTable *table, char *key);
+
 /* inserting macro name and body strings */
-int insert(hashTable* table, char* key, char* value);
+int insert(hashTable *table, char *key, char *value);
+
 /* checking whether a macro named "key" is already present in hash table */
-int contains_key(hashTable* table, char* key);
+int contains_key(hashTable *table, char *key);
+
 /* changing "key" macro body string to "value" parameter */
-int change_value(hashTable* table, char* key, char* value);
+int change_value(hashTable *table, char *key, char *value);
 
 /* same but for hashTableInt (and used for labels' names as keys and IC/DC as value */
 int init_hash_table_int(hashTableInt *table, int size);
+
 int get_value_int(hashTableInt *table, char *key);
+
 int insert_int(hashTableInt *table, char *key, int value, int isData);
+
 int contains_key_int(hashTableInt *table, char *key);
+
 int change_value_int(hashTableInt *table, char *key, int value, int isData);
