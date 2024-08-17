3d7015f25b2645de71a7ef9e76fda3a48e2354b6
DekelMO <dekelm94@gmail.com>
2024-08-17 15:42:13 +0300

    Update pre_assembler.h

--- pre_assembler.h
+++ pre_assembler.h
@@ -4,10 +4,12 @@
 
 /* inserting macros' names and bodies values into hash table (table param) */
 int read_macros_from_file(FILE *file, hashTable *table, int *longestMacroBody);
+
 /* creating .am file and deploying all macros' bodies based on their names from hash table */
 int write_macros_to_file(FILE *readFile, FILE *writeFile, hashTable *table);
+
 /* process of reading from .as file and writing to .am file */
-int deploy_macros(FILE *readFile, FILE *writeFile, hashTable *macroTable, int *longestMacroBodyLength);////////////changed to macroTable
+int deploy_macros(FILE *readFile, FILE *writeFile, hashTable *macroTable, int *longestMacroBodyLength);
 
 /* checking longest macro body for memory allocation in read_macros_from_file */
 void calculate_longest_macro_body(FILE *file, int *longest);
@@ -16,4 +18,4 @@ void calculate_longest_macro_body(FILE *file, int *longest);
 int is_command(char *line);
 
 /* used to check if a line contain a label and save the label name in the array */
-int containLabel(char **labelNamesArray,char *cutCurrentLine,int labelNamesArraySize, int endOfWord);
+int containLabel(char **labelNamesArray, char *cutCurrentLine, int labelNamesArraySize, int endOfWord);
